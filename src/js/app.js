33301c9c71d5c56d558113f2c18d8ca3e1b61d63
davidlucid <davidlucidsl@gmail.com>
2021-01-27 16:04:24 -0600

    Better initial handling of window.web3 and window.ethereum

--- src/js/app.js
+++ src/js/app.js
@@ -249,8 +249,10 @@ App = {
    */
   initWeb3: function() {
     $.getScript("js/web3.min.js", function() {
-      if (typeof web3 !== 'undefined') {
-        App.web3 = new Web3(web3.currentProvider);
+      if (typeof window.ethereum !== 'undefined') {
+        App.web3 = new Web3(window.ethereum);
+      } else if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
+        App.web3 = new Web3(window.web3.currentProvider);
       } else {
         App.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/c52a3970da0a47978bee0fe7988b67b6"));
       }
